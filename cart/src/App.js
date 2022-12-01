c132c9186262edc8fe6f625188d1f974fad2b75d
Adil-khan-007 <adilk9030@gmail.com>
2022-12-01 22:19:22 +0530

    implementation of redux is completed

--- cart/src/App.js
+++ cart/src/App.js
@@ -1,11 +1,11 @@
-// import { Cart } from "./Components/Cart/Cart";
-import {EmptyCart} from "./Components/EmptyCart/EmptyCart"
+import { Cart } from "./Components/Cart/Cart";
+// import {EmptyCart} from "./Components/EmptyCart/EmptyCart"
 
 function App() {
   return (
     <div className="App">
-      <EmptyCart/>
-      {/* <Cart/> */}
+      {/* <EmptyCart/> */}
+      <Cart/>
     </div>
   );
 }
--- cart/src/Components/Cart/Cart.js
+++ cart/src/Components/Cart/Cart.js
@@ -3,27 +3,47 @@ import {useSelector} from "react-redux";
 import "./Cart.css"
 
 export const Cart = ()=>{
-    const data = useSelector((storeData)=>storeData.cart);
-    const [cartData,setCartData] = useState([]);
+    const cartData = useSelector((storeData)=>storeData.cart);
+    const [count,setCount] = useState(1);
+
+    const increaseCount = ()=>{
+        setCount((count)=>count+1);
+    }
+
+    const decreaseCount = ()=>{
+        setCount((count)=>count-1);
+    }
+
     return <div>
         <div className="head">
         <p>Your Shopping Basket</p>
         <button>CHECKOUT SECURELY NOW</button>
         </div>
         <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>ITEMS</th>
-                        <th>PRICE</th>
-                        <th>QUANTITY</th>
-                        <th>SUBTOTAL</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    
-                </tbody>
-            </table>
+            <div className="item-heading">
+                <p style={{width:"530px"}}>ITEMS</p>
+                <p>PRICE</p>
+                <p>QUANTITY</p>
+                <p>SUBTOTAL</p>
+                <p></p>
+            </div>
+            <div>
+                {
+                    cartData.map((el,index)=>{
+                        return <div className = "cartEle" key={index+1}>
+                            <div style={{display:"flex",flexDirection:"row",width:"500px",marginRight:"5px"}}><img src={el.image} width="100px" height="80px" alt={el.title}/>
+                            <div><p>{el.title}</p>
+                            <p>In stock</p></div></div>
+                            <p>{el.price}</p>
+                            <div style={{display:"flex",flexDirection:"row",gap:"10px"}}><button onClick={increaseCount}>+</button>
+                            <p> {count} </p>
+                            <button disabled={count === 1 ? true : false} onClick={decreaseCount}>-</button></div>
+                            <p>{el.price}</p>
+                            <p>*</p>
+                        </div>
+                    })
+                }
+            </div>
         </div>
     </div>
 }
