3683d821f2830afdd87f9b0e7c93edd11e87b228
Anshu Raj <fakereal477@gmail.com>
2022-12-02 15:24:00 +0530

    footer added on about us page

--- frank_body_clone/src/Components/About/About.jsx
+++ frank_body_clone/src/Components/About/About.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Navbar from '../LandingPage/TopSection/Navbar/Navbar';
 import './About.css';
 import Banner from './Banner';
+import {Footer} from '../Footer/footer';
 function About() {
     return (
         <>
             <Navbar />
             <Banner />
+            <Footer />
         </>
     );
 }
