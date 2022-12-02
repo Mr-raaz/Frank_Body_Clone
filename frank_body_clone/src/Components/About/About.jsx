4727eb8c7221fb39c48ea7ca66a3d65b7a5f9760
Anshu Raj <fakereal477@gmail.com>
2022-12-02 14:53:39 +0530

    about us banner created

--- frank_body_clone/src/Components/About/About.jsx
+++ frank_body_clone/src/Components/About/About.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import Navbar from '../LandingPage/TopSection/Navbar/Navbar';
+import './About.css';
+import Banner from './Banner';
 function About() {
     return (
         <>
             <Navbar />
+            <Banner />
         </>
     );
 }
---
+++ frank_body_clone/src/Components/About/Banner.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import banner from './img/banner.png';
+function Banner() {
+    return (
+        <>
+            <div className="banner_outer">
+
+                <div>
+                        <img src={banner} alt="Not found" />
+                </div>
+
+
+                <div className='second_div_about'>
+
+                    <div><h2>About Us</h2></div>
+                     <p><q className='testingQuote'> Frank Body is a one stop shop for all your fashion and lifestyle needs. Being India's largest e-commerce store for fashion and lifestyle products, Myntra aims at providing a hassle free and enjoyable shopping experience to shoppers across the country with the widest range of brands and products on its portal. The brand is making a conscious effort to bring the power of fashion to shoppers with an array of the latest and trendiest products available in the country </q></p>
+                </div>
+
+
+            </div>
+
+
+            <div className="welcomeSection">
+                
+            </div>
+        </>
+    );
+}
+
+export default Banner;
