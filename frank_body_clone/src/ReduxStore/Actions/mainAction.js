7b043d8188cd29d8ea144d6f35b80324d3cbc864
Anshu Raj <106313157+Mr-raaz@users.noreply.github.com>
2022-12-03 18:14:29 +0530

    Delete frank_body_clone directory

--- frank_body_clone/src/ReduxStore/Actions/mainAction.js
+++
@@ -1,6 +0,0 @@
-export default function AddProducts(data , dispatch){
-
-
-   
-
-}
--- frank_body_clone/src/ReduxStore/store.js
+++
@@ -1,3 +0,0 @@
-import { legacy_createStore  , combineReducers} from "redux";
-import ProductReducer from "./Reducers/ProductReducer";
-export const store = legacy_createStore(ProductReducer , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
